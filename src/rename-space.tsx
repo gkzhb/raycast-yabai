100c6cca5bdf43bc20667bd1706f3355ea00e4fa
gkzhb <gkzhb98@gmail.com>
2025-02-09 15:37:46 +0800

    feat: update rename space script, persist labels to file

--- src/rename-space.tsx
+++ src/rename-space.tsx
@@ -1,16 +1,76 @@
-import { useState } from "react";
-import { Action, ActionPanel, List, useNavigation } from "@raycast/api";
+/* eslint-disable no-empty */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { Action, ActionPanel, List, showHUD } from "@raycast/api";
 import { useExec } from "@raycast/utils";
 import { defaultEnv, yabai } from "./constants";
 
+const savedLabelPath = `/Users/${defaultEnv.USER}/.yabai-labels.json`;
+
 export default function RenameSpace() {
   const [input, setInput] = useState("");
+  // new label for the current space
   const [rename, setRename] = useState<{ name: string } | null>(null);
-  const { pop } = useNavigation();
+  // control when to persist space labels to file
+  const [saveLabels, setSaveLabels] = useState(false);
+  // json content to persist space labels
+  const [savedJson, setSavedJson] = useState("");
 
-  useExec(yabai, rename ? ["-m", "space", "--label", rename.name] : ["-v"], {
+  // rename space label
+  useExec(yabai, ["-m", "space", "--label", rename?.name ?? ""], {
     env: defaultEnv,
+    execute: Boolean(rename),
+    onData: () => {
+      setSaveLabels(true);
+    },
+    onError: () => {
+      showHUD("Rename yabai space's label error");
+    },
   });
+
+  // query latest space labels
+  const {
+    data: spacesJson,
+    error,
+    isLoading,
+  } = useExec(yabai, ["-m", "query", "--spaces"], {
+    env: defaultEnv,
+    execute: saveLabels,
+    onError: () => {
+      showHUD("Query yabai spaces error");
+    },
+  });
+
+  // persist labels to json file
+  useExec("cat", [">", savedLabelPath], {
+    input: savedJson,
+    shell: true,
+    execute: Boolean(savedJson),
+    onData: () => {
+      showHUD("Space renamed");
+    },
+    onError: () => {
+      showHUD("Save yabai spaces' labels error");
+    },
+  });
+
+  useEffect(() => {
+    if (!saveLabels || error || !spacesJson || isLoading) {
+      return;
+    }
+    try {
+      // parse and filter spaces with non-empty labels
+      const spaces = JSON.parse(spacesJson);
+      const labeledSpaces = spaces
+        .filter((item: Record<string, any>) => item.label?.length)
+        .map((item: Record<string, any>) => ({
+          index: item.index,
+          label: item.label,
+        }));
+
+      setSavedJson(JSON.stringify(labeledSpaces));
+    } catch {}
+  }, [spacesJson, error, saveLabels, isLoading]);
 
   return (
     <List
@@ -19,11 +79,10 @@ export default function RenameSpace() {
       actions={
         <ActionPanel>
           <Action
-            title={"Rename space"}
+            title="Rename Space Label"
             shortcut={{ key: "enter", modifiers: ["ctrl"] }}
             onAction={() => {
               setRename({ name: input });
-              pop();
             }}
           />
         </ActionPanel>
